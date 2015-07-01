1752eaaed1a2977eb79f300b9b1b81d0b0ac6696
chijuzipi <yuzhoubrother@gmail.com>
2015-07-01 00:15:53 -0700

    dropdown added to main page

--- js/main.js
+++ js/main.js
@@ -5,15 +5,11 @@ $(document).ready(function(){
   var productObject = Parse.Object.extend("Product");
   var query = new Parse.Query(productObject);
   var url = window.location.pathname; 
-
+  
+  var cataArray = ['Handbag', 'Cloth', 'Cosmetics', 'Nutrition', 'Jewel', 'Baby', 'Other'];
   var dict = {};
-  dict["bag.html"] = "Handbag";
-  dict["jew.html"] = "Jewel";
-  dict["nut.html"] = "Nutrition";
-  dict["cos.html"] = "Cosmetics";
-  dict["baby.html"] = "Baby";
-  dict["clo.html"] = "Cloth";
-  dict["oth.html"] = "Other";
+  buildDict(dict);
+
 
   //console.log(url);
   var divid = url.split('/')
@@ -33,62 +29,7 @@ $(document).ready(function(){
 
       for (i = results.length-1; i >= 0; i--){
         //console.log("line12: "+ results[i].id);
-        var name   = results[i].get('product_name');
-        var price  = results[i].get('product_price'); 
-        var desc   = results[i].get('product_desc'); 
-        var type   = results[i].get('product_type'); 
-
-        var init = 0;
-        var linkArray  = [];
-        var imageName = 'image_'+init;
-        var image = results[i].get(imageName);
-
-        while(image!=null && image!=undefined){
-          linkArray.push(image.url());
-          init++;
-          imageName = 'image_'+init;
-          image = results[i].get(imageName);
-        }
-
-        var link   = linkArray[0];
-        //console.log("line31 " + results[i].id);
-        //console.log(link);
-
-        var modalId = results[i].id;
-
-        var doc = "<div class='col-sm-4 col-lg-4 col-md-4'><div class='thumbnail'><img src='" + link + "'" +
-                  "alt='' data-toggle='modal' data-target='#"+ modalId + "'>" + 
-                  "<hr><div class='caption'><h4 class='pull-right'>¥: " + price + 
-                  "</h4><h4><a href='#' data-toggle='modal' data-target='#" + modalId + "'>" + name + 
-                  "</a></h4><p>" + desc + "</p>";
-
-        var modal = 
-              "<div class='modal fade' id='" + modalId + "' role='dialog'>" + 
-              "<div class='modal-dialog'>" + 
-              "<div class='modal-content'>" + 
-              "<div class='modal-header'>"  + 
-              "<button type='button' class='close' data-dismiss='modal'>&times;</button>" + 
-              "<h4 class='modal-title'>" + name + "</h4>" + 
-              "</div>" +
-              "<div class='modal-body'>";
-
-        for(j=0; j < linkArray.length-1; j++){
-          modal += "<img src='"+ linkArray[j] + "' data-dismiss='modal'><hr>";
-        }
-
-        modal += 
-          "<img src='"+ linkArray[j] + "' data-dismiss='modal'>" + 
-          "</div>" + 
-          "<div class='modal-footer'>" +
-          "<button type='button' class='btn btn-default' data-dismiss='modal' style='height:50px; width:100%'>Close</button>" + 
-          "</div>" +
-          "</div>" + 
-          "</div>" + 
-          "</div>";
-
-        $("#productS").append(doc);
-        $("#modalContainer").append(modal);
-
+        generateContent(results[i]);
       }
     },
 
@@ -96,6 +37,108 @@ $(document).ready(function(){
       console.log("failed to get object");
     }
   });
+
+  $('.sub').click(function(){
+    var cata = $(this).attr('id');
+
+    for (i = 0; i < cataArray.length; i++){
+      var id = '.' + cataArray[i];
+      if(cataArray[i] == cata){
+        $(id).show();
+      }
+      else{
+        $(id).hide();
+      }
+    }
+
+    generateDropdown(cata);
+    
+  });
+
+  function generateDropdown(cata){
+    var brandArray = dict[cata];
+    var doc = "<select id='brandSelect'>"
+    var brand = '';
+    for (i = 0; i < brandArray.length; i++){
+      brand = brandArray[i]; 
+      doc += "<option value='" + brand + "'>" + brand + "</option>";
+    }
+    doc += "</select>";
+         
+  }
+
+  function buildDict(dict){
+    
+    dict['Handbag']      = ['Handbag', '可爱包包', '所有', 'Coach', 'MK', 'Guess', '其他'];
+    dict['Cloth']        = ['时尚衣服', '所有', 'Tommy', 'Carters', 'A&F', 'Levis', '其他'];
+    dict['Jewel']        = ['可爱首饰', '所有', '其他'];
+    dict['Nutrition']    = ['健康营养', '所有', 'GNC', 'MoveFree', '其他'];
+    dict['Cosmetics']    = ['高级化妆', 'Lancome', 'Clinique', '其他'];
+    dict['Baby']         = ['放心婴幼儿用品', '所有', '其他'];
+    dict['Other']        = ['丰富多采'];
+  }
+
+  function generateContent(result){
+      var name   = result.get('product_name');
+      var price  = result.get('product_price'); 
+      var desc   = result.get('product_desc'); 
+      var type   = result.get('product_type'); 
+
+      var init = 0;
+      var linkArray  = [];
+      var imageName = 'image_'+init;
+      var image = result.get(imageName);
+
+      while(image!=null && image!=undefined){
+        linkArray.push(image.url());
+        init++;
+        imageName = 'image_'+init;
+        image = result.get(imageName);
+      }
+
+      var link   = linkArray[0];
+      //console.log("line31 " + results[i].id);
+      //console.log(link);
+
+      var modalId = result.id;
+
+      var doc = "<div class='col-sm-4 col-lg-4 col-md-4 " + type + 
+                "'><div class='thumbnail'><img src='" + link + "'" +
+                "alt='' data-toggle='modal' data-target='#"+ modalId + "'>" + 
+                "<hr><div class='caption'><h4 class='pull-right'>¥: " + price + 
+                "</h4><h4><a href='#' data-toggle='modal' data-target='#" + modalId + "'>" + name + 
+                "</a></h4><p>" + desc + "</p>";
+
+      var modal = 
+            "<div class='modal fade' id='" + modalId + "' role='dialog'>" + 
+            "<div class='modal-dialog'>" + 
+            "<div class='modal-content'>" + 
+            "<div class='modal-header'>"  + 
+            "<button type='button' class='close' data-dismiss='modal'>&times;</button>" + 
+            "<h4 class='modal-title'>" + name + "</h4>" + 
+            "</div>" +
+            "<div class='modal-body'>";
+
+      for(j=0; j < linkArray.length-1; j++){
+        modal += "<img src='"+ linkArray[j] + "' data-dismiss='modal'><hr>";
+      }
+
+      modal += 
+        "<img src='"+ linkArray[j] + "' data-dismiss='modal'>" + 
+        "</div>" + 
+        "<div class='modal-footer'>" +
+        "<button type='button' class='btn btn-default' data-dismiss='modal' style='height:50px; width:100%'>Close</button>" + 
+        "</div>" +
+        "</div>" + 
+        "</div>" + 
+        "</div>";
+
+      $("#productS").append(doc);
+      $("#modalContainer").append(modal);
+
+    
+  }
+
 
 });
 
